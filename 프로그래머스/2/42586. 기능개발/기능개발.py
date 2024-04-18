a0031e05975e5ba2d7af387779bc46949fdc6b7f
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-04-18 09:47:15 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,20 @@
 import math
 def solution(progresses, speeds):
     answer = []
-    n = len(speeds)
-    remaining_days = []
-    for i in range(n):
-        remaining_days.append(math.ceil((100-progresses[i])/speeds[i]))
-    
+    days = [0] * len(progresses)
     cnt = 1
-    standard = remaining_days[0]
-    for i in range(1, n):
-        if standard >= remaining_days[i]:
+    
+    for i in range(len(progresses)):
+        days[i] = math.ceil((100-progresses[i]) / speeds[i])
+    
+    standard = days[0]
+    for i in range(1, len(days)):
+        if days[i] <= standard:
             cnt += 1
         else:
             answer.append(cnt)
+            standard = days[i]
             cnt = 1
-            standard = remaining_days[i]
+    
     answer.append(cnt)
-    # print(remaining_days)
     return answer
