d6edd4371b548f4d623f839a385a1d411960c22e
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-06-19 12:37:34 +0900

    [Silver II] Title: 나무 자르기, Time: 640 ms, Memory: 134992 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,32 +1,58 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main
-{
-    static boolean isPossible(int[] heights, int cutHeight, int thresholdHeight) {
-        long sum = 0;
-        for (int h : heights)
-            if (h > cutHeight) sum += h - cutHeight;
-        return sum >= thresholdHeight;
-    }
+public class Main {
 
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] h = new int[N];
-        for (int i = 0; i < N; i++)
-            h[i] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int l = 0, r = 1000000000, ans = -1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (isPossible(h, m, M)) {
-                ans = m;
-                l = m + 1;
-            }
-            else r = m - 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // H 높이 나무 모두 절단
+        Integer[] height = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
         }
+
+        int ans = findOptimalHeight(height, 0, 1000000000, M);
+
         System.out.println(ans);
+
+        br.close();
     }
+
+    public static int findOptimalHeight(Integer[] height, int l, int r, int x) {
+        int ans = -1;
+
+        while (l <= r) {
+            // 임의의 절단기 높이
+            int m = (l + r) / 2;
+
+            // 가져갈 수 있는 나무 길이 구하기
+            long sum = 0;
+            for (Integer integer : height) {
+                if (integer > m) {
+                    sum += integer - m;
+                }
+            }
+
+            // 원하는 만큼 가져갈 수 있다면 높이 높여본다
+            if (sum >= x) {
+                l = m + 1;
+                ans = m;
+            } else {    // 원하는 만큼 못 가져가면 높이 낮춰본다
+                r = m - 1;
+            }
+        }
+
+        return ans;
+
+    }
+
 }
