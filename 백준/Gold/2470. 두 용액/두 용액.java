3835950fb7a9218bd17be6f1ffc97a976a695920
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-06-25 09:38:43 +0900

    [Gold V] Title: 두 용액, Time: 400 ms, Memory: 29900 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,12 +2,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -27,47 +22,29 @@ public class Main {
 
         Arrays.sort(array);
 
+        int leftIdx = 0;
+        int rightIdx = N-1;
         int ansAbs = Integer.MAX_VALUE;
-        int ans1 = 0;
-        int ans2 = 0;
-        for (int i = 0; i < N - 1; i++) {
-            int pairValue = findPairValue(array, i + 1, N - 1, array[i]);
-            int abs = Math.abs(array[i] + pairValue);
-            if (abs < ansAbs) {
-                ansAbs = abs;
-                ans1 = array[i];
-                ans2 = pairValue;
+        int ansLeft = 0, ansRight = 0;
+
+        while (leftIdx < rightIdx) {
+            int currentSum = array[leftIdx] + array[rightIdx];
+            int currentAbs = Math.abs(currentSum);
+            if (ansAbs > currentAbs) {
+                ansAbs = currentAbs;
+                ansLeft = leftIdx;
+                ansRight = rightIdx;
             }
+            if (currentSum > 0) rightIdx--;
+            else leftIdx++;
         }
 
-        System.out.println(ans1 + " " + ans2);
+
+        System.out.printf("%d %d", array[ansLeft], array[ansRight]);
         br.close();
     }
 
-    public static int findPairValue(Integer[] array, int start, int end, int std) {
-        int l = start;
-        int r = end;
-        int min = Integer.MAX_VALUE;
-        int min_idx = 0;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            int sum = array[m] + std;
-            if (sum == 0) {
-                return array[m];
-            } else if (sum < 0) {
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
 
-            if (Math.abs(sum) < min) {
-                min = Math.abs(sum);
-                min_idx = m;
-            }
-        }
-
-        return array[min_idx];
-    }
 
 
 }
