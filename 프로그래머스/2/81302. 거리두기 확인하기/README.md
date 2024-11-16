e9f40ac3d5f79bb6055e0556826bd94ede880204
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-11-16 09:52:14 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.06 ms
+메모리: 79.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 09:47:53
+2024년 11월 16일 09:52:11
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            boolean allRight = true;
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    char cur = places[i][j].charAt(k);
+                    if (cur == 'P') {
+                        allRight = search(places[i], j, k);
+                        if (!allRight) {
+                            answer[i] = 0;
+                            break;
+                        }
+                    }
+                }
+                if (!allRight) break;
+            }
+            if (allRight) {
+                answer[i] = 1;
+            }
+        }
+        return answer;
+    }
+    
+    public boolean search(String[] place, int x, int y) {
+        boolean[][] visited = new boolean[5][5];
+        int[] dxs = new int[]{-1, 0, 1, 0};
+        int[] dys = new int[]{0, 1, 0, -1};
+        
+        visited[x][y] = true;
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dxs[i];
+            int ny = y + dys[i];
+            
+            // 다음 칸이 5 x 5 범위 밖이거나 칸막이라면 더 조사할 필요 없음 -> 무조건 지킴
+            if (notInRange(nx, ny) || place[nx].charAt(ny) == 'X') continue;
+            
+            // 바로 다음 칸에 사람이 있으면 -> 거리두기 안 지킴 -> 해당 대기실은 0
+            if (place[nx].charAt(ny) == 'P') return false;
+            
+            // 다음 칸이 책상이라면 한 칸 더 조사해야 된다 
+            for (int j = 0; j < 4; j++) {
+                int nnx = nx + dxs[j];
+                int nny = ny + dys[j];
+                
+                if (notInRange(nnx, nny)) continue;
+                if (visited[nnx][nny]) continue;
+                
+                // 이전 칸은 책상이고 다음 칸은 사람이라면 거리 두기 안 지킴 -> 해당 대기실 0
+                if (place[nnx].charAt(nny) == 'P') return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean notInRange(int nx, int ny) {
+        return nx < 0 || nx >= 5 || ny < 0 || ny >= 5;
+    }
+}
