3fb7c84eff9aa913b05e47da5df4a93b2f0b8c52
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-11-19 20:36:38 +0900

    [Silver I] Title: 안전 영역, Time: 232 ms, Memory: 18836 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,78 +5,69 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] graph;
-    static boolean[][] visited;
-    static int n;
-    static int maxHeight = Integer.MIN_VALUE;
-    static int[] dx = new int[]{1, 0, -1, 0};
-    static int[] dy = new int[]{0, 1, 0, -1};
-    static int amount;
+  static int n;
+  static int[][] graph;
+  static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    n = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
+    graph = new int[n][n];
+    int maxH = Integer.MIN_VALUE;
 
-        graph = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                maxHeight = Math.max(maxHeight, graph[i][j]);
-            }
-        }
-
-        int answer = 1;
-
-        for (int i = 1; i <= maxHeight; i++) {
-            amount = i;
-            visited = new boolean[n][n];
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (!visited[j][k] && graph[j][k] > amount) {
-                        dfs(j, k);
-                        count++;
-                    }
-                }
-            }
-            answer = Math.max(count, answer);
-        }
-        System.out.println(answer);
-        br.close();
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < n; j++) {
+        graph[i][j] = Integer.parseInt(st.nextToken());
+        maxH = Math.max(maxH, graph[i][j]);
+      }
     }
 
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
+    int ans = Integer.MIN_VALUE;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i], ny = y + dy[i];
-            if (canGo(nx, ny)) {
-                dfs(nx, ny);
-            }
+    for(int i = 0; i <= maxH; i++) {
+      visited = new boolean[n][n];
+      int count = 0;
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+          if (!visited[j][k] && graph[j][k] > i) {
+            count++;
+            dfs(j, k, i);
+          }
         }
+      }
+      ans = Math.max(ans, count);
     }
 
+    System.out.println(ans);
+  }
 
-    static boolean canGo(int nx, int ny) {
-        if (!inRange(nx, ny)) {
-            return false;
-        }
+  static int[] dxs = new int[]{1, 0 , -1, 0};
+  static int[] dys = new int[]{0, 1 , 0, -1};
 
-        // 이미 방문했거나, 잠긴 지역이라면 못감
-        if (visited[nx][ny] || graph[nx][ny] <= amount) {
-            return false;
-        }
+  static void dfs(int x, int y, int h) {
+    visited[x][y] = true;
 
-        return true;
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dxs[i], ny = y + dys[i];
+      if (canGo(nx, ny, h)) {
+        dfs(nx, ny, h);
+      }
     }
+  }
 
-    /**
-     * n x n 범위 안에 있는지 확인하는 함수
-     */
-    static boolean inRange(int nx, int ny) {
-        return 0 <= nx && nx < n && 0 <= ny && ny < n;
-    }
+  static boolean canGo(int nx, int ny, int h) {
+    // n x n 범위 밖이거나 방문한 적 있거나 높이가 h 이하면 못감
+    if (!inRange(nx, ny)) return false;
+    if (visited[nx][ny] || graph[nx][ny] <= h) return false;
+
+    return true;
+  }
+
+  static boolean inRange(int nx, int ny) {
+    return 0 <= nx && nx < n && 0 <= ny && ny < n;
+  }
+
 }
