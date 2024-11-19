0d8fe5aff90bab0363bffdef0717a2d7ef2dbafd
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-11-19 14:06:39 +0900

    [Silver I] Title: 용돈 관리, Time: 232 ms, Memory: 22440 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,57 +1,72 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+  static int n;
+  static int m;
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+  static int[] money;
 
-        Integer[] amount = new Integer[N];
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            amount[i] = sc.nextInt();
-            sum += amount[i];
-        }
+  public static void main(String[] args) throws IOException {
+    // M번 인출
+    // K원 인출, 하루 다 보낼 수 있으면 그대로 사용, 모자라면 남은 금액 입금 & K원 인출
+    // K를 최소화하자
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
 
-
-        int ans = -1;
-        int l = 1, r = sum;
-        while (l <= r) {
-            int m = (l + r) / 2;
-
-            if (isPossible(amount, m, M)) {
-                r = m - 1;
-                ans = m;
-            } else {
-                l = m + 1;
-            }
-        }
-
-        System.out.println(ans);
-
+    money = new int[n];
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+      money[i] = Integer.parseInt(br.readLine());
+      total += money[i];
     }
 
-    private static boolean isPossible(Integer[] array, int m, int M) {
-        int count = 1;
-        int m_copy = m;
-        for (Integer amount : array) {
-            if (amount > m) return false;
+    int start = 0, end = total + 1;
+    int ans = -1;
+    while (start <= end) {
+      // mid가 k원을 의미함
+      int mid = (start + end) / 2;
 
-            if (amount > m_copy) {
-                if (count == M) return false;   // 이미 M번 채웠을 때
-                count++;
-                m_copy = m;
-            }
-            m_copy -= amount;
-
-        }
-
-        return true;
+      if (canUse(mid)) {
+        ans = mid;
+        end = mid - 1;
+      } else {  // k원이 작아서 m번 이상 인출하거나, 하루에 쓸 돈 조차 커버가 안됨
+        start = mid + 1;
+      }
     }
 
+    System.out.println(ans);
+
+  }
+
+  static boolean canUse(int oneDay) {
+    int count = 1;
+    int cur = oneDay;
+
+    for (int amount : money) {
+      // 하루 쓸 돈이 인출한 금액보다 크면 안됨
+      if (amount > oneDay) return false;
+
+      // 오늘 쓸 돈이 남은 돈보다 많음
+      if (amount > cur) {
+        // 이미 M번 인출함
+        if (count == m) return false;
+
+        count++;
+        cur = oneDay;
+      }
+
+      cur -= amount;
+    }
+
+    return true;
+  }
 
 }
+
