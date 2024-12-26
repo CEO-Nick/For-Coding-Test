d821123edd3bdad3d90e42edf15581d06dc69b67
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-12-26 10:33:59 +0900

    Title: 삽입 정렬 구현, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241226/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
