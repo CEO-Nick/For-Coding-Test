f2cf6ce6ebffde345099b531c2f2de683cf66a2c
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-12-26 10:18:09 +0900

    Title: 거품 정렬 구현, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241226/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        // n-1-i 까지인 이유 : 맨 마지막 자리가 정해짐
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
