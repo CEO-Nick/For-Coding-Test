8ff34b65cadc2ee76d8c2dc4613be3ff2ff4afeb
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-12-27 09:35:47 +0900

    Title: 금 채굴하기, Time: 106ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241227/금 채굴하기/gold-mining.java
+++ 241227/금 채굴하기/gold-mining.java
@@ -33,7 +33,7 @@ public class Main {
             }
         }
 
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         k = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
