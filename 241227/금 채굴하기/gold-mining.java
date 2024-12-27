981fee812f7317303350630ccb6f08bd24da21c0
nick <123347183+CEO-Nick@users.noreply.github.com>
2024-12-27 10:20:55 +0900

    Title: 금 채굴하기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241227/금 채굴하기/gold-mining.java
+++ 241227/금 채굴하기/gold-mining.java
@@ -2,6 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static class Point {
+        int x;
+        int y;
+        int d;
+
+        Point(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+    }
+
     static int[][] grid;
     static boolean[][] visited;
     static int n;
@@ -33,26 +45,20 @@ public class Main {
             }
         }
 
-        int maxK = 0;
-        int am = 0;
-        while (am < n*n) {
-            am = maxK * maxK + ((maxK+1) * (maxK+1));
-            maxK++;
-        }
 
         int max = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                for (int l = 0; l <= n*2+1; l++) {
+                for (int l = 0; l <= n; l++) {
                     k = l;
                     int amount = k * k + ((k+1) * (k+1));
                     visited = new boolean[n][n];
                     gold = 0;
-                    dfs(i, j, 0);
+                    bfs(i, j);
                     if (gold * m - amount >= 0) {
                         max = Math.max(max, gold);
-                    //    System.out.println("i : " + i + "\tj : " + j + "\tk : " + k + "\tgold : " + gold);
                     } 
+                    
                 }
             }
         }
@@ -62,19 +68,28 @@ public class Main {
 
     }
 
-    public static void dfs(int x, int y, int depth) {
+    public static void bfs(int x, int y) {
+        Queue<Point> q = new ArrayDeque<>();
+        q.add(new Point(x,y,0));
         visited[x][y] = true;
-        if (grid[x][y] == 1) gold++;
 
-        if (depth == k) return;
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            if (grid[cur.x][cur.y] == 1) gold++;
+            if (cur.d == k) continue;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dxs[i];
-            int ny = y + dys[i];
-            // 격자 벗어남
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            
-            if (!visited[nx][ny]) dfs(nx, ny, depth + 1);
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dxs[i];
+                int ny = cur.y + dys[i];
+                // 격자 벗어남
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.add(new Point(nx, ny, cur.d + 1));
+                }
+            }
         }
+        
     }
 }
