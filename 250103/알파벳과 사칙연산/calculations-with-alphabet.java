6dc43c90cff277cc6b5db7a615589705f08a243e
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-03 10:37:47 +0900

    Title: 알파벳과 사칙연산, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250103/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+
+    static char[] input;
+    static int size;
+    static int max = 0;
+
+    static void recur(int idx) {
+        if (idx >= size) {
+            // System.out.println(input);
+            int res = calculate();
+            max = Math.max(max, res);
+            return;
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            char tmp = input[idx];
+            input[idx] = (char)(i + 48);    // '0' = 48
+
+            recur(idx + 2);
+
+            input[idx] = tmp;
+        }
+    }
+
+    static int calculate() {
+        int cur = input[0] - '0';
+        for (int i = 1; i < size - 1; i += 2) {
+            switch (input[i]) {
+                case '-':
+                    cur -= input[i+1] - '0';
+                    break;
+                case '+':
+                    cur += input[i+1] - '0';
+                    break;
+                case '*':
+                    cur *= input[i+1] - '0';
+                    break;
+            }
+        }
+
+        return cur;
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        input = sc.next().toCharArray();
+        size = input.length;
+
+        recur(0);
+        System.out.println(max);
+        // copy();
+        // System.out.println(tmp);
+        // System.out.println(size);
+    }
+}
