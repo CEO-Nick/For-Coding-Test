a23df7ef85fb603909b13f960019e0a5c23b8090
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-03 11:44:12 +0900

    Title: 알파벳과 사칙연산, Time: 162ms, Memory: 11MB, Status: Passed - CodeTree

--- 250103/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 250103/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -6,35 +6,36 @@ public class Main {
     static int size;
     static int max = Integer.MIN_VALUE;
 
+    static Map<Character, Integer> map;
+    static Set<Character> alph;
+    static List<Character> list;
+
     static void recur(int idx) {
-        if (idx >= size) {
+        if (idx == size) {
             int res = calculate();
             max = Math.max(max, res);
             return;
         }
 
         for (int i = 1; i <= 4; i++) {
-            char tmp = input[idx];
-            input[idx] = (char)(i + 48);    // '0' = 48
-
-            recur(idx + 2);
-
-            input[idx] = tmp;
+            map.put(list.get(idx), i);
+            recur(idx+1);
+            map.remove(list.get(idx));
         }
     }
 
     static int calculate() {
-        int cur = input[0] - '0';
-        for (int i = 1; i < size - 1; i += 2) {
+        int cur = map.get(input[0]);
+        for (int i = 1; i < input.length - 1; i += 2) {
             switch (input[i]) {
                 case '-':
-                    cur -= input[i+1] - '0';
+                    cur -= map.get(input[i+1]);
                     break;
                 case '+':
-                    cur += input[i+1] - '0';
+                    cur += map.get(input[i+1]);
                     break;
                 case '*':
-                    cur *= input[i+1] - '0';
+                    cur *= map.get(input[i+1]);
                     break;
             }
         }
@@ -45,7 +46,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         input = sc.next().toCharArray();
-        size = input.length;
+        
+        alph = new HashSet<>();
+        for (int i = 0; i < input.length; i += 2) {
+            alph.add(input[i]);
+        }
+        list = new ArrayList<>(alph);
+        map = new HashMap<>();
+        size = list.size();
 
         recur(0);
         System.out.println(max);
