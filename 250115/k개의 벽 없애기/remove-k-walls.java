6480c2769e9d876843a38a0935dc602d35d51e1d
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-15 19:38:17 +0900

    Title: k개의 벽 없애기, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250115/k개의 벽 없애기/remove-k-walls.java
+++ 250115/k개의 벽 없애기/remove-k-walls.java
@@ -13,31 +13,31 @@ public class Main {
             this.order = order;
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Point point = (Point) o;
-            return x == point.x && y == point.y;
-        }
+        // @Override
+        // public boolean equals(Object o) {
+        //     if (this == o) {
+        //         return true;
+        //     }
+        //     if (o == null || getClass() != o.getClass()) {
+        //         return false;
+        //     }
+        //     Point point = (Point) o;
+        //     return x == point.x && y == point.y;
+        // }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+        // @Override
+        // public int hashCode() {
+        //     return Objects.hash(x, y);
+        // }
 
-        @Override
-        public String toString() {
-            return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                ", order=" + order +
-                '}';
-        }
+        // @Override
+        // public String toString() {
+        //     return "Point{" +
+        //         "x=" + x +
+        //         ", y=" + y +
+        //         ", order=" + order +
+        //         '}';
+        // }
         
     }
 
