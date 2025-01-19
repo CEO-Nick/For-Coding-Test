5e6c4a31b9a028962cb1f3a3f8a93ca5b9f37e70
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-19 14:38:23 +0900

    Title: 계단 오르기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 250119/계단 오르기/climbing-stairs.java
+++ 250119/계단 오르기/climbing-stairs.java
@@ -1,41 +1,21 @@
 import java.util.*;
 
 public class Main {
-
-    static int climb(int x) {
-        if (dp[x] != 0) return dp[x];
-        if (x == 2 || x == 3) return 1;
-        if (x < 2) return 0;
-        dp[x] = climb(x-2) + climb(x-3);
-        return dp[x];
-    }
-
-    static int[] dp;
-    static int n;
-
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        // dp[5] = dp[3] + dp[2]
-        // dp[3] = 1
-        // dp[2] = 1
-        // dp[1] = 0
-        // dp[0] = 0
-        n = sc.nextInt();
-        dp = new int[1010];
+
+        int n = sc.nextInt();
+        int[] dp = new int[1010];
         dp[3] = 1;
         dp[2] = 1;
         dp[1] = 0;
         dp[0] = 0;
+        
         for (int i = 4; i <= 1000; i++) {
             dp[i] = (dp[i-2] + dp[i-3]) % 10007;
         }
 
-
-        // int res = climb(n);
-        // for (int i = 0; i <= n; i++) {
-        //     System.out.println(i + ": " + dp[i]);
-        // }
         System.out.println(dp[n]);
     }
 }
