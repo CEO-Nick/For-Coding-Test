ed20ade7b9174b90831ef6c89be197143cb90434
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-24 09:29:31 +0900

    Title: 최대 증가 부분 수열, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 250124/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 250124/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -19,14 +19,19 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }    
 
+        // dp[i] << i번째 숫자의 최대 증가 부분 수열의 원소 수 
+        // arr = 1, 6, 4, 3, 9, 3 
+        // dp =  1, 2, 2, 2, 3, 2
         int[] dp = new int[n];
         
+        // 모두 1로 초기화 : 각 좌표에서 부분 수열 시작할 때 최소한 자기 자신은 포함하니깐
         for (int i = 0; i < n; i++) {
             dp[i] = 1;
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
+                // 현재 값보다 작은 경우에만 dp 갱신 (증가하는 부분 수열이어야 하니까)
                 if (arr[j] < arr[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
