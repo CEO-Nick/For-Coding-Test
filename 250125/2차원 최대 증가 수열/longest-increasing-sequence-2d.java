8deec85938dd862c31d2da0da689ae3adad950e3
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-25 11:45:09 +0900

    Title: 2차원 최대 증가 수열, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250125/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static void DP(int curx, int cury) {
+        for (int i = 0; i < curx; i++) {
+            for (int j = 0; j < cury; j++) {
+                if (grid[i][j] < grid[curx][cury]) {
+                    dp[curx][cury] = Math.max(dp[curx][cury], dp[i][j] + 1);
+                }
+            }
+        }
+    }
+
+    static void init() {
+        dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -100;
+            }
+        }
+        dp[0][0] = 1;
+    }
+
+    
+    static int[] dxs = new int[] {1, 0, -1, 0};
+    static int[] dys = new int[] {0, 1, 0, -1};
+    static int n;
+    static int m;
+    static int[][] grid;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.    
+        // 현재 값보다 큰 칸으로만 점프 가능
+        // 점프할 때 무조건 (1, 1) 이상 
+
+        // 다른 칸에서 자신의 칸으로 올 수 있는 경우의 수 찾아서 업데이트
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        grid = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        init();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                DP(i, j);
+            }
+        }
+        
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
