0a2aa98df72bb943ae9bf640563e7818784ea808
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-26 15:05:32 +0900

    Title: 알바로 부자 되기, Time: 173ms, Memory: 10MB, Status: Passed - CodeTree

--- 250126/알바로 부자 되기/being-rich-by-working-part-time.java
+++ 250126/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -13,16 +13,8 @@ public class Main {
             end = e;
             money = m;
         }
-
-        @Override
-        public String toString() {
-            return "Work{" +
-                "start=" + start +
-                ", end=" + end +
-                ", money=" + money +
-                '}';
-        }
     }
+
     public static void main(String[] args) throws IOException {
         // Please write your code here.    
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,7 +22,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         
         ArrayList<Work> list = new ArrayList<>();
-        int[] dp = new int[n];
+        int[] dp = new int[n];  // 알바 i할 때 얻을 수 있는 최대 금액
 
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
@@ -40,13 +32,14 @@ public class Main {
             int m = Integer.parseInt(st.nextToken());
 
             list.add(new Work(s, e, m));
-            dp[i] = m;
+            dp[i] = m;  // 알바 i만 했을 때 벌 수 있는 돈
         }
 
         dp[0] = list.get(0).money;
         for (int i = 1; i < n; i++) {
             Work cur = list.get(i);
             for (int j = 0; j < i; j++) {
+                // 알바 j를 할 수 있다면(cur과 안겹친다면) (현재 dp값)과 (알바 j했을 때 dp값 + 현재 알바 금액) 을 비교한다
                 if (list.get(j).end < cur.start) {
                     dp[i] = Math.max(dp[i], dp[j] + cur.money);
                 }
