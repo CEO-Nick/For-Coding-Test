8c98d43525bf588114ec6faa26eafa440a4585d6
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-01-29 22:51:31 +0900

    Title: 부분 수열의 합이 m, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 250129/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
+++ 250129/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        // Please write your code here.    
+        // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -26,22 +26,25 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[m+1];
-        
-        for(int i = 1;i<=m;i++){
+        int[] dp = new int[m + 1];
+
+        for (int i = 1; i <= m; i++) {
             dp[i] = 100000;
         }
 
-        for(int i = 0;i<n;i++){
-            for(int j = m;j>=1;j--){
-                if(j - arr[i] >= 0){
-                    dp[j] = Math.min(dp[j],dp[j-arr[i]]+1);
+        for (int i = 0; i < n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (j - arr[i] >= 0) {
+                    dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
                 }
             }
         }
 
-        if(dp[m] == 100000) System.out.println(-1);
-        else System.out.println(dp[m]);
+        if (dp[m] == 100000) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[m]);
+        }
 
     }
 }
