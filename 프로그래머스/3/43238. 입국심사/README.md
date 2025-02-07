643a041c9bfa1cc97f66c4e8dc9e5d1448122cb1
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-02-07 11:08:25 +0900

    [level 3] Title: 입국심사, Time: 105.28 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 92.5 MB, 시간: 42.96 ms
+메모리: 99.4 MB, 시간: 105.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 18:43:27
+2025년 02월 07일 11:08:22
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,32 +2,46 @@ import java.util.*;
 import java.math.*;
 
 class Solution {
-    public long solution(int n, int[] times) {
-        long answer = 0;
-        long left = 0L;
-        long right = 0L;
-        for (int t : times) {
-            right = Math.max(right, t);
-        }
-        right *= n;
-
+    static int N;
+    static int maxTime;
+    static int[] TIMES;
+    static long answer = Long.MAX_VALUE;
+    
+    static void binarySearch() {
+        long left = 0L, right = 1000000000000000000L;
+        
         while (left <= right) {
-            long mid = (left + right) / 2;
-            long people = 0L;
-            for (int time : times) {
-                people += (mid / time);
-                if (people > n) {
-                    break;
-                }
-            }
+            long mid = (left + right) / 2L;  // mid는 걸린 시간 값을 말함
+            System.out.println("left: " + left + "\tright: " + right + "\tmid: " + mid);
             
-            if (people >= n) {
-                answer = mid;
+            long result = calc(mid);
+            System.out.println("result: " + result);
+            
+            // mid 시간동안 처리할 수 있는 사람의 수가 N명보다 많은 경우 -> 시간을 줄여본다. 근데 정답일 수도 있으니 정답 갱신도 진행
+            if (result >= N) {
                 right = mid - 1;
+                answer = Math.min(answer, mid);
             } else {
                 left = mid + 1;
-            }
+            }           
         }
+    }
+    
+    // time 동안 처리할 수 있는 사람의 수
+    static long calc(long time) {
+        long res = 0L;
+        for (int t : TIMES) {
+            res += time / (long) t;
+        }
+        
+        return res;
+    }
+        
+        
+    public long solution(int n, int[] times) {
+        N = n;
+        TIMES = times;
+        binarySearch();
         
         return answer;
     }
