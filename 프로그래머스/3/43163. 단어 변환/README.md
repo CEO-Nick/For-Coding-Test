a7ea9ce47c4cafb5e901f5b6df78a97ae260f96a
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-03-02 20:28:00 +0900

    [level 3] Title: 단어 변환, Time: 0.56 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 1.96 ms
+메모리: 76.1 MB, 시간: 0.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 09:35:13
+2025년 03월 02일 20:27:58
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,65 +1,50 @@
 import java.util.*;
 
 class Solution {
-    class Word {
+    
+    static class Word {
         String value;
         int depth;
         
-        Word(String v, int d) {
-            this.value = v;
-            this.depth = d;
+        Word(String value, int depth) {
+            this.value = value;
+            this.depth = depth;
         }
     }
     
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        
-        // words에 없으면 변환 불가능
-        boolean exists = false;
-        for (String w : words) {
-            if (target.equals(w)) exists = true;
-        }
-        if (!exists) return 0;
-        
+        // begin -> target
+        // 한 번에 1개의 알파벳만 바꿀 수 있음. 근데 그 단어 words에 있어야 함
         boolean[] visited = new boolean[words.length];
         
-        Queue<Word> q = new LinkedList<>();
-        q.offer(new Word(begin, 0));
-        
+        ArrayDeque<Word> q = new ArrayDeque<>();
+        q.add(new Word(begin, 0));
         while (!q.isEmpty()) {
             Word cur = q.poll();
-            
-            if (cur.value.equals(target)) {
-                answer = cur.depth;
-                break;
+            if (target.equals(cur.value)) {
+                return cur.depth;
             }
             
             for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canTransfer(cur.value, words[i])) {
-                    q.add(new Word(words[i], cur.depth + 1));
+                if (!visited[i] && canConvert(cur.value, words[i])) {
                     visited[i] = true;
+                    q.add(new Word(words[i], cur.depth + 1));
                 }
-            }   
-            
-        }
-        
-        
-        
-        
-        return answer;
-    }
-    
-    public boolean canTransfer(String from, String to) {
-        int cnt = 0;
-        boolean can = true;
-        for (int i = 0; i < from.length(); i++) {
-            if (from.charAt(i) != to.charAt(i)) cnt++;
-            if (cnt > 1) {
-                can = false;
-                break;
             }
         }
         
-        return can;
+        return 0;
+    }
+    
+    // a -> b로 변환 가능한지
+    public boolean canConvert(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            char aa = a.charAt(i), bb = b.charAt(i);
+            if (aa != bb) diff++;
+            if (diff >= 2) return false;
+        }
+        
+        return true;
     }
 }
