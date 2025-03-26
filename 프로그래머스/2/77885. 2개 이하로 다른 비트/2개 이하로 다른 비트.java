c7a06cccb229ccab7824aee16fd54649c113af4c
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-03-26 09:50:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 78.97 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,32 +1,24 @@
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
+        int n = numbers.length;
+        long[] answer = new long[n];
         
-        for (int i = 0; i < numbers.length; i++) {
-            long num = numbers[i];
-            String binary = null;
-
-            // 짝수인 경우 -> RSB만 0 -> 1로 수정하면 된다
-            if (num % 2 == 0) answer[i] = num + 1;
-            else {
-                
-                // 홀수인 경우 -> 오른쪽에서 0이 처음 나오는 index찾고, 01 -> 10 으로 수정하면 된다
-                binary = Long.toBinaryString(num);
-                int idx = binary.lastIndexOf("0");
-                
-                // 못찾은 경우 -> 1111과 같은 0이 없는 경우 -> 맨 왼쪽에 0이있다고 가정
-                if (idx == -1) {
-                    binary = "10" + binary.substring(1, binary.length());
-                } else {
-                    binary = binary.substring(0, idx) + "10" + binary.substring(idx + 2, binary.length());
-                }
-                answer[i] = Long.parseLong(binary, 2);      
+        String bin;
+        for(int i = 0; i < n; i++) {
+            long cur = numbers[i];
+            if (cur % 2 == 0) {
+                answer[i] = cur+1;
+                continue;
             }
             
+            // 홀수인 경우는 우측에서 첫번째 01 을 10으로 수정 (ex. 1(01)1 -> 1(10)1)
+            bin = Long.toBinaryString(cur);
+            int idx = bin.lastIndexOf('0');
+            if (idx == -1) bin = "10" + bin.substring(1, bin.length());
+            else bin = bin.substring(0, idx) + "10" + bin.substring(idx + 2, bin.length());
             
+            answer[i] = Long.parseLong(bin, 2);
         }
         return answer;
     }
-    
-    
 }
