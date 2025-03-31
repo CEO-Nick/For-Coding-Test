52a8e7fc900aa9efec1dcb5299354fdc687dd84f
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-03-31 11:07:21 +0900

    [level 2] Title: 호텔 대실, Time: 6.55 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,43 +1,17 @@
 import java.util.*;
 
 class Solution {
-    static class Event implements Comparable{
-        int time;
-        boolean isCheckIn;
-        
-        Event(int t, boolean c) {
-            this.time = t;
-            this.isCheckIn = c;
-        }
-        
-        public int compareTo(Object obj) {
-            Event e = (Event) obj;
-            // 같은 시간이면 checkout 먼저 처리
-            if (this.time == e.time) {
-                return this.isCheckIn ? 1 : -1;
-            }
-            return this.time - e.time;
-        }
-    }
-    
     public int solution(String[][] book_time) {
         int answer = 0;
-        List<Event> list = new ArrayList<>();
-        
+        int[] room = new int[1440];
         for (int i = 0; i < book_time.length; i++) {
             int inTime = toMinute(book_time[i][0]);
             int outTime = Math.min(toMinute(book_time[i][1]) + 10, 1439);
-            list.add(new Event(inTime, true));
-            list.add(new Event(outTime, false));
+            for (int k = inTime; k < outTime; k++) room[k]++;
         }
-        Collections.sort(list);
         
-        int cnt = 0;
-        for (Event e : list) {
-            if (e.isCheckIn) cnt++;
-            else cnt--;
-            
-            answer = Math.max(answer, cnt);
+        for (int i = 0; i < 1440; i++) {
+            answer = Math.max(answer, room[i]);
         }
         return answer;
     }
