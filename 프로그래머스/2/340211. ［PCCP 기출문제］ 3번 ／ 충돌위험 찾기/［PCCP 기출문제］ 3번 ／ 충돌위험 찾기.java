76cb830059c8373cd745615027ae84ee6e0896c0
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-05-05 10:55:17 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 212.06 ms, Memory: 152 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+class Solution {
+    static class Point {
+        int x;
+        int y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Point point = (Point) obj;
+            return x == point.x && y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+    
+    static HashMap<Integer, Point> pointMap;
+    static ArrayList<Point>[] routeList;
+    
+    public int solution(int[][] points, int[][] routes) {
+        int answer = 0;
+        pointMap = new HashMap<>();
+        
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i][0] - 1;
+            int y = points[i][1] - 1;
+            pointMap.put(i+1, new Point(x, y));
+        }
+        
+        // idx: 로봇 번호 arraylist: 로봇의 방문 좌표 리스트
+        routeList = new ArrayList[routes.length];
+        for (int i = 0; i < routeList.length; i++) {
+            routeList[i] = new ArrayList<Point>();
+        }
+        
+        // 가장 긴 경로 
+        int maxLen = -1;
+        for (int i = 0; i < routes.length; i++) {
+            recordFullRoute(i, routes[i]);
+            maxLen = Math.max(maxLen, routeList[i].size());
+        }
+        
+        // 매 초마다 각 좌표에 있는 로봇 개수
+        HashMap<Point, Integer> countMap = new HashMap<>();
+        for (int j = 0; j < maxLen; j++) {
+            countMap.clear();
+            for (int i = 0; i < routeList.length; i++) {
+                if (j >= routeList[i].size()) continue;
+                Point p = routeList[i].get(j);
+                countMap.put(p, countMap.getOrDefault(p, 0) + 1);
+            }
+            
+            for (int count : countMap.values()) {
+                if (count >= 2) answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    
+    public static void recordFullRoute(int robotIdx, int[] path) {
+        for (int i = 0; i < path.length - 1; i++) {
+            int curP = path[i];
+            int nextP = path[i+1];
+            
+            if (i == 0) {
+                routeList[robotIdx].add(pointMap.get(curP));
+            }
+            
+            recordRoute(robotIdx, curP, nextP);
+        }
+    }
+    
+    // route의 인덱스, 시작 포인트, 끝 포인트
+    public static void recordRoute(int idx, int sp, int ep) {
+        Point startPoint = pointMap.get(sp);
+        int startX = startPoint.x;
+        int startY = startPoint.y;
+        
+        Point endPoint = pointMap.get(ep);
+        int endX = endPoint.x;
+        int endY = endPoint.y;
+        
+        int curX = startX;
+        int curY = startY;
+        
+        while (curX != endX) {
+            int dx = endX > startX ? 1 : -1;
+            curX += dx;
+            routeList[idx].add(new Point(curX, curY));
+        }
+        
+        while (curY != endY) {
+            int dy = endY > startY ? 1 : -1;
+            curY += dy;
+            routeList[idx].add(new Point(curX, curY));
+        }
+    }
+}
