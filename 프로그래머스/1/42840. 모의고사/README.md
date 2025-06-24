69f0bb67c120ad6565de6d7c1c3f9141f7eb76a2
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-06-24 13:52:12 +0900

    [level 1] Title: 모의고사, Time: 22.24 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 3.04 ms
+메모리: 93.4 MB, 시간: 22.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 12:12:09
+2025년 06월 24일 13:52:09
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,26 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] sum = new int[3];
-        int[] std1 = new int[]{1,2,3,4,5};
-        int[] std2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] std3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+    static int NUM = 3;
     
+    public int[] solution(int[] answers) {
+        int[] idx = new int[NUM];
+        int[][] student = new int[][] { 
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] cnt = new int[NUM];
         
         for (int i = 0; i < answers.length; i++) {
-            sum[0] += answers[i] == std1[i % 5] ? 1 : 0;
-            sum[1] += answers[i] == std2[i % 8] ? 1 : 0;
-            sum[2] += answers[i] == std3[i % 10] ? 1 : 0;
+            for (int j = 0; j < NUM; j++) {
+                int index = i % student[j].length;
+                if (answers[i] == student[j][index]) cnt[j]++;
+            }
         }
         
-        int maxSum = Math.max(sum[0], Math.max(sum[1], sum[2]));
-        List<Integer> ans = new ArrayList<>();
-        
-        for (int i = 0; i < 3; i++) {
-            if (sum[i] == maxSum) ans.add(i+1);
+        // cnt에서 최댓값 찾고, 최댓값인 index로 새 배열 만들기
+        int max = Arrays.stream(cnt).max().getAsInt();
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < NUM; i++) {
+            if (cnt[i] == max) answer.add(i+1);
         }
         
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
