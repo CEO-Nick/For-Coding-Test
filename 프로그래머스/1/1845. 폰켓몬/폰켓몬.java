80ad6a796e07f9a56ef56414e417c27001c9a6a1
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-02 14:18:38 +0900

    [level 1] Title: 폰켓몬, Time: 4.60 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,14 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         HashSet<Integer> set = new HashSet<>();
-        
         for (int num : nums) set.add(num);
         
-        if (nums.length / 2 <= set.size()) answer = nums.length / 2;
-        else answer = set.size();
-        
-        return answer;
+        return Math.min(set.size(), nums.length / 2);        
     }
 }
