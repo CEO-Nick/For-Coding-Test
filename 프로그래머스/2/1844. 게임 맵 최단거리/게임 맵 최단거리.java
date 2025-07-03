871c3844a04e6659d9a37eedd28b62bdace3138c
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-03 17:22:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.93 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,78 +1,68 @@
 import java.util.*;
 
 class Solution {
-    static class Point {
-        int x;
-        int y;
-        int move;
+    /*
+        동서남북 한 칸씩 이동
+        맵 내에서 이동
+        벽은 못 지나감
+        못 도착할 수도 있음
         
-        Point(int x, int y, int move) {
+        최단거리 -> BFS 활용
+        visited 배열에 칸의 개수 최소값 넣기
+    */
+    
+    static class Point {
+        int x, y;
+        int cnt;
+        
+        Point (int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.move = move;
+            this.cnt = cnt;
         }
     }
     
-    static boolean[][] visited;
     static int[] dxs = new int[] {1, 0, -1, 0};
     static int[] dys = new int[] {0, 1, 0, -1};
     
-    static void BFS() {
+    static int N;
+    static int M;
+    
+    static int[][] visited;
+    public int solution(int[][] maps) {
+        int answer = 0;
+        N = maps.length;
+        M = maps[0].length;
+        
+        visited = new int[N][M];
+                
         ArrayDeque<Point> q = new ArrayDeque<>();
-        q.add(new Point(0, 0, 1));
-        visited[0][0] = true;
-        answer[0][0] = 1;
+        q.addLast(new Point(0, 0, 1));
+        visited[0][0] = 1;
         
         while (!q.isEmpty()) {
-            Point cur = q.poll();    
-            
-            if (cur.x == n-1 && cur.y == m-1) {
-                answer[cur.x][cur.y] = Math.min(answer[cur.x][cur.y], cur.move);
-                return;
-            }
+            Point p = q.poll();
             
             for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dxs[i], ny = cur.y + dys[i];
-                if (canGo(nx, ny)) {
-                    visited[nx][ny] = true;
-                    q.add(new Point(nx, ny, cur.move + 1));
-                    answer[nx][ny] = answer[nx][ny] == 0 ? cur.move + 1 : Math.min(answer[nx][ny], cur.move + 1);
+                int nx = p.x + dxs[i];
+                int ny = p.y + dys[i];
+                
+                // 격자 밖이거나 벽인 경우 pass
+                if (!inRange(nx, ny)) continue;
+                if (maps[nx][ny] == 0) continue;
+                
+                if (visited[nx][ny] == 0) {
+                    q.addLast(new Point(nx, ny, p.cnt+1));
+                    visited[nx][ny] = visited[p.x][p.y] + 1;                    
                 }
             }
         }
         
-    }
-    
-    static boolean canGo(int nx, int ny) {
-        // 범위 밖인 경우
-        if (!inRange(nx, ny)) return false;
-        
-        // 방문했거나 벽인 경우
-        if (visited[nx][ny] || grid[nx][ny] == 0) return false;
-        
-        return true;
+        answer = visited[N-1][M-1] == 0 ? -1 : visited[N-1][M-1];
+        return answer;
     }
     
     static boolean inRange(int nx, int ny) {
-        return 0 <= nx && nx < n && 0 <= ny && ny < m;
-    }
-    
-    static int[][] answer;
-    static int[][] grid;
-    static int n;
-    static int m;
-    
-    public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        grid = maps;
-        visited = new boolean[n][m];
-        answer = new int[n][m];
-        
-        BFS();
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(answer[i]));
-        }
-        return answer[n-1][m-1] == 0 ? -1 : answer[n-1][m-1];
+        return 0 <= nx && nx < N && 0 <= ny && ny < M;
     }
 }
