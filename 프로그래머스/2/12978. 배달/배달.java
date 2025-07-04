b628da72febff15bedac81e3b90d4ad9602925ac
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-04 11:14:46 +0900

    [level 2] Title: 배달, Time: 13.95 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,73 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static int INF = 500001;
+    
     static class Node {
-        int town;
+        int num;
         int cost;
-        int before;
         
-        Node(int t, int c, int b) {
-            this.town = t;
-            this.cost = c;
-            this.before = b;
-        }
-        
-        public String toString() {
-            return "now: " + town + " cost: " + cost + " before: " + before;
+        Node (int num, int cost) {
+            this.num = num;
+            this.cost = cost;
         }
     }
     
-    static int[][] grid;
-    static int[] answer;
+    static ArrayList<Node>[] adjList;
     
     public int solution(int N, int[][] road, int K) {
-        // 1번 마을 기준으로 모든 도시까지 최단거리 구하기
-        // K 이하인 마을 개수 구하기
-        grid = new int[N+1][N+1];
-        answer = new int[N+1];
-
-        for (int i = 0; i <= N; i++) Arrays.fill(grid[i], INF);
-        Arrays.fill(answer, INF);
+        adjList = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
         
         for (int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
+            int n1 = road[i][0];
+            int n2 = road[i][1];
             int cost = road[i][2];
-            // 조건에 도로는 여러 개 있을 수 있다고 함 -> 그 중 최소 비용 도로만 저장
-            grid[a][b] = Math.min(grid[a][b], cost);
-            grid[b][a] = Math.min(grid[b][a], cost);
+            adjList[n1].add(new Node(n2, cost));
+            adjList[n2].add(new Node(n1, cost));   
         }
         
-        bfs(N);
+        int[] minCost = new int[N+1];
+        Arrays.fill(minCost, Integer.MAX_VALUE);
         
-        int res = 0;
-        for (int i = 1; i <= N; i++) {
-            if (answer[i] <= K) res++;
-        }
-        return res;
-    }
-    
-    public static void bfs(int N) {
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(1, 0, 0));
-        answer[1] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.cost, n2.cost));
+        pq.add(new Node(1, 0));
+        minCost[1] = 0;
         
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            int now = cur.town;
-            int cost = cur.cost;
-            for (int i = 1; i <= N; i++) {
-                if (grid[now][i] == INF) continue;
-                if (cur.before == i) continue;
-                
-                int calcCost = answer[now] + grid[now][i];
-                if (calcCost < answer[i]) {
-                    answer[i] = calcCost;
-                    q.add(new Node(i, calcCost, now));
-                }
+        boolean[] visited = new boolean[N+1];
+        
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
             
+            if (visited[cur.num]) continue;
+            visited[cur.num] = true;
+            
+            for (Node next : adjList[cur.num]) {
+                
+                
+                if (minCost[next.num] > minCost[cur.num] + next.cost) {
+                    minCost[next.num] = minCost[cur.num] + next.cost;
+                    pq.add(new Node(next.num, minCost[next.num]));
+                }
             }
         }
+        
+        int answer = 0;
+        for (int cost : minCost) {
+            if (cost <= K) answer++;
+        }
+
+        return answer;
     }
 }
