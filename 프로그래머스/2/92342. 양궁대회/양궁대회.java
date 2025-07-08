358c0df97f5b6ce74a118505e1205a3b14c1d5c7
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-08 10:42:58 +0900

    [level 2] Title: 양궁대회, Time: 4.70 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,61 +1,55 @@
-import java.util.*;
-
 class Solution {
-    static int N;
     static int[] ryan;
     static int[] apeach;
     static int[] answer;
-    static int max = -1;
     
+    static int N;
+    static int MAX = Integer.MIN_VALUE;
     
-    // idx : 점수 (10-idx) 점
-    public void dp(int count, int start) {
-        if (count == N) {
-            int res = compare();
-            if (res > max) {
+    static void dp(int idx, int cnt) {
+        if (cnt == N) {
+            int diff = compare();
+            if (diff > MAX) {
                 answer = ryan.clone();
-                max = res;
+                MAX = diff;
             }
             return;
         }
         
-        // 10 - i 인 이유는 낮은 점수부터 조합 만들기 위해서
-        for (int i = start; i <= 10; i++) {
-            if (apeach[10-i] < ryan[10-i]) continue;  // 이미 ryan이 이겼는데 거기에 화살을 추가할 필요 없음
-            ryan[10-i]++;
-            dp(count+1, i);
-            ryan[10-i]--;
+        for (int i = idx; i >= 0; i--) {
+            // 이미 라이언이 이김
+            if (ryan[i] > apeach[i]) continue;
+            ryan[i]++;
+            dp(i, cnt+1);
+            ryan[i]--;
         }
-        
-        
     }
     
-    // ryan 이기면 점수 차이 반환 | 지면 -1 반환
-    public int compare() {
-        int a_score = 0;
-        int r_score = 0;
-        for (int i = 0; i <= 10; i++) {
+    // 라이언이 이기면 점수차이 반환, 어피치가 이기면 -1 반환
+    private static int compare() {
+        int ryanScore = 0;
+        int apeachScore = 0;
+        for (int i = 0; i <= N; i++) {
             if (apeach[i] == 0 && ryan[i] == 0) continue;
-            else if (apeach[i] >= ryan[i]) a_score += (10 - i);
-            else r_score += (10 - i);
+            
+            if (apeach[i] >= ryan[i]) {
+                apeachScore += (10 - i);
+            } else {
+                ryanScore += (10 - i);
+            }
         }
         
-        return r_score > a_score ? (r_score - a_score) : -1;
+        int diff = ryanScore - apeachScore;
+        return diff > 0 ? diff : -1;
     }
     
     public int[] solution(int n, int[] info) {
-        N = n;
-        ryan = new int[11];
-        answer = new int[11];
         apeach = info;
+        ryan = new int[11];
+        N = n;
         
-        dp(0, 0);
+        dp(10, 0);
         
-        System.out.println(Arrays.toString(answer));
-        System.out.println(max);
-        if (max == -1) {
-            answer = new int[] {-1};
-        }
-        return answer;
+        return MAX == -1 ? new int[] {-1} : answer;
     }
 }
