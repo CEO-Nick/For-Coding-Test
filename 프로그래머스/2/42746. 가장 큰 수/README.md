20ad443d252e8fa54b06e8887e8b5e78feaff164
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-10 08:30:25 +0900

    [level 2] Title: 가장 큰 수, Time: 300.76 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 192.51 ms
+메모리: 122 MB, 시간: 300.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 10:58:41
+2025년 07월 10일 08:30:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,31 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers) {        
-        List<String> list = new ArrayList<>();
-        int sum = 0;
-        for (int num : numbers) {
-            list.add(String.valueOf(num));
-            sum += num;
-        }
-        // numbers 배열이 모두 0인 경우
-        if (sum == 0) return "0";
+    
+    public String solution(int[] numbers) {
+        ArrayList<String> strList = new ArrayList<>();
+        for (int num : numbers) strList.add(String.valueOf(num));
         
-        list.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String n1 = o1 + o2;
-                String n2 = o2 + o1;
-                return n2.compareTo(n1);
-            }
+        Collections.sort(strList, (s1, s2) -> {
+            int a = Integer.parseInt(s1 + s2);
+            int b = Integer.parseInt(s2 + s1);
+            return Integer.compare(b, a);
         });
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 0 && list.get(i).equals("0")) continue;
-            sb.append(list.get(i));
-        }
-        
-        return sb.toString();
+        for (String str : strList) sb.append(str);
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
