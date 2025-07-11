353fa04a965d96c56de2fd83b765513ed3b6364e
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-11 14:00:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 320.74 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,26 +4,19 @@ class Solution {
     // return: 공평하게 자르는 방법 수 (없으면 0)
     public int solution(int[] topping) {
         int answer = 0;
-        HashMap<Integer, Integer> cheolsu = new HashMap<>();
-        HashMap<Integer, Integer> brother = new HashMap<>();
+        HashSet<Integer> cheolsu = new HashSet<>();
+        HashMap<Integer, Integer> brother = new HashMap<>(); 
         
-        cheolsu.put(topping[0], 1);
-        
-        for (int i = 1; i < topping.length; i++) {
+        for (int i = 0; i < topping.length; i++) {
             int count = brother.getOrDefault(topping[i], 0);
             brother.put(topping[i], count+1);
         }
-        
-        answer += cheolsu.size() == brother.size() ? 1 : 0;
-        
-        for (int i = 1; i < topping.length; i++) {
+                
+        for (int i = 0; i < topping.length; i++) {
+            cheolsu.add(topping[i]);
             
-            int count1 = cheolsu.getOrDefault(topping[i], 0);
-            cheolsu.put(topping[i], count1 + 1);
-            
-            int count2 = brother.get(topping[i]);
-            if (count2 == 1) brother.remove(topping[i]);
-            else brother.put(topping[i], count2-1);
+            brother.put(topping[i], brother.getOrDefault(topping[i], 0) - 1);
+            if (brother.get(topping[i]) == 0) brother.remove(topping[i]);
             
             answer += cheolsu.size() == brother.size() ? 1 : 0;
         }
