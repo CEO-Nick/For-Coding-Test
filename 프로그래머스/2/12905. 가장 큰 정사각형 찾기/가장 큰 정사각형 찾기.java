554e9eb28ad9e4575930ed3ff5cd3965a61c4eae
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-17 10:36:20 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 17.07 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,27 +1,32 @@
+import java.util.*;
+
 class Solution
 {
     public int solution(int [][]board)
     {
-        int answer = 0;
         
         int n = board.length;
         int m = board[0].length;
-
-        if (n == 1 && m == 1) {
-            if (board[0][0] == 0) return 0;
-            else return 1;
+        int[][] dp = new int[n][m];
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = board[i][0];
+            max = Math.max(max, dp[i][0]);
         }
-            
-        // board[i][j] = 4 -> (i, j)를 우측 하단 꼭짓점으로 하는 정사각형의 가능한 최대 변의 길이가 4이다
+        
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = board[0][j];
+            max = Math.max(max, dp[0][j]);
+        }
+        
+        
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (board[i][j] == 0) continue;
-                // 좌측, 상단, 좌측 대각선 값 중 최솟값 + 1이 현재 좌표에서 만들 수 있는 최대 정사각형 한 변의 길이
-                board[i][j] = Math.min(Math.min(board[i][j-1], board[i-1][j]), board[i-1][j-1]) + 1;
-                answer = Math.max(answer, board[i][j]);
+                dp[i][j] = Math.min((Math.min(dp[i-1][j-1], dp[i-1][j])), dp[i][j-1]) + 1;
+                max = Math.max(max, dp[i][j]);
             }
         }
-        
-        return answer * answer;
+        return max * max;
     }
 }
