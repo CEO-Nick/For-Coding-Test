a9e125ed37ceca6dce17317df491abce2a9a14fd
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-22 09:36:13 +0900

    [level 1] Title: 실패율, Time: 11.53 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,47 +1,22 @@
 import java.util.*;
 
 class Solution {
-    class Challenger implements Comparable{
-        int idx;
-        double rate;
-        
-        Challenger(int idx, double rate) {
-            this.idx = idx;
-            this.rate = rate;
-        }
-        
-        public int compareTo(Object obj) {
-            Challenger c = (Challenger) obj;
-            if (Double.compare(c.rate, this.rate) == 0) {
-                return this.idx - c.idx;
-            }
-            return Double.compare(c.rate, this.rate);
-        }
-        
-        public String toString() {
-            return "(" + idx + ", " + rate + ")";
-        }
-    }
-    
     public int[] solution(int N, int[] stages) {
         int[] challenges = new int[N+2];
         
-        int total = stages.length;
         for (int stg : stages) challenges[stg]++;
                 
-        ArrayList<Challenger> list = new ArrayList<>();
+        HashMap<Integer, Double> failMap = new HashMap<>();
+        double total = stages.length;
         for (int i = 1; i <= N; i++) {
-            if (total == 0) list.add(new Challenger(i, 0));
-            else list.add(new Challenger(i, (double)challenges[i] / total));
+            if (challenges[i] == 0) failMap.put(i, 0.0);
+            else failMap.put(i, challenges[i] / total);
             total -= challenges[i];
         }
         
-        Collections.sort(list);
-        // System.out.println(list);
-        int[] answer = new int[N];
-        for (int i = 0; i < N; i++) {
-            answer[i] = list.get(i).idx;
-        }
-        return answer;
+        return failMap.entrySet().stream()
+            .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
+            .mapToInt(HashMap.Entry::getKey)
+            .toArray();
     }
 }
