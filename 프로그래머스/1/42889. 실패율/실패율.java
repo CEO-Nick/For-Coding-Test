c8e5e856f1e499b98755154d33836eb99ccf4401
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-22 09:31:15 +0900

    [level 1] Title: 실패율, Time: 5.13 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,27 +1,47 @@
 import java.util.*;
 
 class Solution {
+    class Challenger implements Comparable{
+        int idx;
+        double rate;
+        
+        Challenger(int idx, double rate) {
+            this.idx = idx;
+            this.rate = rate;
+        }
+        
+        public int compareTo(Object obj) {
+            Challenger c = (Challenger) obj;
+            if (Double.compare(c.rate, this.rate) == 0) {
+                return this.idx - c.idx;
+            }
+            return Double.compare(c.rate, this.rate);
+        }
+        
+        public String toString() {
+            return "(" + idx + ", " + rate + ")";
+        }
+    }
     
     public int[] solution(int N, int[] stages) {
-        int[] user = new int[N+2];
+        int[] challenges = new int[N+2];
         
-        for (int i = 0; i < stages.length; i++) {
-            user[stages[i]]++;
-        }
-        
-        Map<Integer, Double> fails = new HashMap<>();
-        double total = stages.length;
+        int total = stages.length;
+        for (int stg : stages) challenges[stg]++;
+                
+        ArrayList<Challenger> list = new ArrayList<>();
         for (int i = 1; i <= N; i++) {
-            if (user[i] == 0) {
-                fails.put(i, 0.);
-                continue;
-            }
-            
-            fails.put(i, user[i] / total);
-            total -= user[i];
+            if (total == 0) list.add(new Challenger(i, 0));
+            else list.add(new Challenger(i, (double)challenges[i] / total));
+            total -= challenges[i];
         }
         
-        
-        return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+        Collections.sort(list);
+        // System.out.println(list);
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = list.get(i).idx;
+        }
+        return answer;
     }
 }
