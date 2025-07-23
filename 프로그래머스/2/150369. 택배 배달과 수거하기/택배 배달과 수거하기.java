61d28637281530549f9f2ee470d5e39882596a49
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-23 10:47:29 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 18.48 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,34 +1,63 @@
-import java.util.*;
-
 class Solution {
+    // 항상 cap만큼 챙겨서 배달이나 수거해야 되는 곳까지 
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = 0L;
-        
-        for (int i = pickups.length - 1; i >= 0;) {
-            
-            // 배달/수거 모두 할 일 없는 집일 때만 pass
-            if (deliveries[i] == 0 && pickups[i] == 0) {
-                i--;
-                continue;
-            }
-            
-            calc(deliveries, cap, i);
-            calc(pickups, cap, i);
-            
-            answer += (i + 1) * 2;
-        }
-        return answer;
-    }
-    
-    static void calc(int[] arr, int cap, int idx) {
-        while (idx >= 0) {
-            if (cap >= arr[idx]) {
-                cap -= arr[idx];
-                arr[idx--] = 0;
-            } else {
-                arr[idx] -= cap;
+        long answer = 0;
+        int maxD = 0;
+        int maxP = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if (deliveries[i] != 0) {
+                maxD = i;
                 break;
+            } 
+        }
+        for (int i = n-1; i >= 0; i--) {
+            if (pickups[i] != 0) {
+                maxP = i;
+                break;
+            } 
+        }
+        
+        if (maxD == 0 && maxP == 0) return 0;
+        
+        while (maxD >= 0 || maxP >= 0) {
+            int move = Math.max(maxD, maxP);
+            answer += (move + 1) * 2;
+
+            int curCap = cap;
+            // 배달하기
+            while (maxD >= 0) {
+                if (curCap > deliveries[maxD]) {
+                    curCap -= deliveries[maxD];
+                    deliveries[maxD] = 0;
+                    maxD--;
+                } else if (curCap == deliveries[maxD]) {
+                    curCap = 0;
+                    deliveries[maxD] = 0;
+                    maxD--;
+                } else {
+                    deliveries[maxD] -= curCap;
+                    break;
+                }
+            }
+
+            // 수거하기
+            curCap = cap;
+            while (maxP >= 0) {
+                if (curCap > pickups[maxP]) {
+                    curCap -= pickups[maxP];
+                    pickups[maxP] = 0;
+                    maxP--;
+                } else if (curCap == pickups[maxP]) {
+                    curCap = 0;
+                    pickups[maxP] = 0;
+                    maxP--;
+                } else {
+                    pickups[maxP] -= curCap;
+                    break;
+                }
             }
         }
+        
+        return answer;
     }
 }
