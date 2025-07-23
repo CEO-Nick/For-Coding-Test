ff5108e9cf668aa31c2c03887bbc1c7a8ce6f73a
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-23 09:30:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.01 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,89 +1,37 @@
 import java.util.*;
 
 class Solution {
-    
-     class Privacy {
-        String term;
-        Integer year;
-        Integer month;
-        Integer day;
-        
-        Privacy(String t, String date) {
-            this.term = t;
-            this.year = Integer.parseInt(date.substring(0,4));
-            this.month = Integer.parseInt(date.substring(5,7));
-            this.day = Integer.parseInt(date.substring(8,10));
-        }
-        
-        Integer endPrivacy(int duration) {
-            int plusMonth = this.month + duration;
-            if (plusMonth % 12 == 0) {
-                this.month = 12;
-                this.year += (plusMonth / 12) - 1;
-            } else {
-                this.month = plusMonth % 12;
-                this.year += (plusMonth) / 12;
-            }
-            int temp = this.day - 1;
-            if (temp == 0) {
-                this.month--;
-                this.day = 28;
-                if (this.month == 0) {
-                    this.year--;
-                    this.month = 12;
-                }
-            } else {
-                this.day = temp;
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            sb.append(String.valueOf(this.year));
-            if (this.month < 10) {
-                sb.append(0);
-            }
-            sb.append(String.valueOf(this.month));
-            if (this.day < 10) {
-                sb.append(0);
-            }
-            sb.append(String.valueOf(this.day));
-            return Integer.parseInt(sb.toString());
-        }
-        
-    }
-    
+    // 수집 일자 + 유효기간 <= 오늘 날짜 -> 파기
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        
-        // 모든 달은 28일까지
-        // 파기해야될 개인정보 번호를 오름차순으로
-        today = today.replace(".", "");
-        int td = Integer.parseInt(today);
-        System.out.println(td);
-        
-        String[] term;
+        int now = dateToInt(today);
+
         HashMap<String, Integer> termMap = new HashMap<>();
-        
-        for (int i = 0; i < terms.length; i++) {
-            term = terms[i].split(" ");
-            termMap.put(term[0], Integer.parseInt(term[1]));
+        for (String term : terms) {
+            String[] input = term.split(" ");
+            termMap.put(input[0], Integer.parseInt(input[1]) * 28);
         }
         
-        String[] ps;
-        
-        for (int i = 0; i < privacies.length; i++) {
-            ps = privacies[i].split(" ");
-            
-            Privacy p = new Privacy(ps[1], ps[0]);
-            int endDate = p.endPrivacy(termMap.get(ps[1]));
-            if (endDate < td) answer.add(i+1);
+        int idx = 1;
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (String privacy : privacies) {
+            String[] p = privacy.split(" ");
+            int past = dateToInt(p[0]);
+            int end = past + termMap.get(p[1]);
+
+            if (end <= now) answer.add(idx);
+            idx++;
         }
         
-        int[] arr = answer.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
-        
-        return arr;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
     
+    public static int dateToInt(String date) {
+        String[] dates = date.split("\\.");
+        int year = Integer.parseInt(dates[0]);
+        int month = Integer.parseInt(dates[1]);
+        int day = Integer.parseInt(dates[2]);
+        
+        return day + (month * 28) + (year * 12 * 28);
+    }
     
 }
