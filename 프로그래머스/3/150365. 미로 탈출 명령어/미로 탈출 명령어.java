d67a87506f840315fe873a94d60e50ca8506bbe1
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-23 12:21:53 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.83 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,54 +1,48 @@
 import java.util.*;
 
 class Solution {
+    // 이동거리가 k여야 함
+    // 중복 방문 가능
+    // 탈출 경로를 문자열로 나타내면 사전순 경로로 탈출
+    // d -> l -> r -> u
     
-    static int K;
-    static int N;
-    static int M;
-    static int R;
-    static int C;
-    
-    
+    class Point {
+        int x;
+        int y;
+        int cnt;
+        String route;
+        Point(int x, int y, int cnt, String route) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+            this.route = route;
+        }
+    }
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        int distance = Math.abs(x-r) + Math.abs(y-c);
+        int diff = k - distance;
         
-        N = n;
-        M = m;
-        R = r;
-        C = c;
-        K = k;
-        
-        // x,y <-> r,c 의 맨하튼 거리(=최소거리) 구하기
-        int dist = Math.abs(r-x) + Math.abs(c-y);
-        int diff = k - dist;
-        
-        // 최소거리 이동 거리보다 k 값 작거나, 최소거리로 목적지 도착한 후에 왕복 못하는 경우
-        // diff 값이 홀수면 도착지에서 왔다갔다를 할 수 없음
         if (diff < 0 || diff % 2 == 1) return "impossible";
         
-        // 애초에 사전순으로 움직이자 (d > l > r > u)
         StringBuilder sb = new StringBuilder();
-        while (K-- > 0) {
-            int[] next = next(x, y);
-            x += next[0];
-            y += next[1];
-            sb.append((char)next[2]);
+        while (k-- > 0) {
+            // 범위에 벗어나지 않고, 해당 방향으로 움직였을 때 거리가 가까워진 거라면 이동하기
+            if (x + 1 <= n && Math.abs(x+1 - r) + Math.abs(y-c) <= k) {
+                x++;
+                sb.append("d");
+            } else if (y - 1 >= 1 && Math.abs(x-r) + Math.abs(y-1-c) <= k) {
+                y--;
+                sb.append("l");
+            } else if (y+1 <= n && Math.abs(x-r) + Math.abs(y+1-c) <= k) {
+                y++;
+                sb.append("r");
+            } else {
+                x--;
+                sb.append("u");
+            }
         }
-        
-        
         
         return sb.toString();
     }
-    
-    static int[] next(int x, int y) {
-        if (x + 1 <= N && Math.abs(x+1 - R) + Math.abs(y - C) <= K) {
-            return new int[] {1, 0, 'd'};
-        } else if (y - 1 >= 1 && Math.abs(x - R) + Math.abs(y-1 - C) <= K ) {
-            return new int[] {0, -1, 'l'};
-        } else if (y + 1 <= M && Math.abs(x - R) + Math.abs(y+1 - C) <= K ) {
-            return new int[] {0, 1, 'r'};
-        } else {
-            return new int[] {-1, 0, 'u'};
-        }
-    }
-    
 }
