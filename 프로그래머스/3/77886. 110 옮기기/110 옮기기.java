67acd12ad3e38d213967961b6c161acedd23c472
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-24 10:45:56 +0900

    [level 3] Title: 110 옮기기, Time: 3536.89 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -10,6 +10,7 @@ class Solution {
             sb = new StringBuilder();
             int cnt = 0;
             
+            // target에서 "110" 뽑아내고 개수 세기
             for (int j = 0; j < target.length(); j++) {
                 int sbLen = sb.length();
                 if (sbLen >= 2 && target.charAt(j) == '0') {
@@ -22,6 +23,8 @@ class Solution {
                 sb.append(target.charAt(j));                
             }
         
+            // 뽑아내고 난 문자열에서 마지막에서 첫번째로 발견되는 0 뒤에 "110" 붙이는게 가장 작은 경우
+            // 0이 발견되지 않으면 idx = -1로 맨앞에 "110"을 붙이면 된다
             int idx = -1;
             for (int j = sb.length()-1; j >= 0; j--) {
                 if (sb.charAt(j) == '0') {
