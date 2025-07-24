3efc2598eed43e570eb1dea30df348e7d6f4f809
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-24 10:08:24 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.85 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -7,6 +7,8 @@ class Solution {
         grid = arr;
         int length = arr.length;
         int res = divide(0, 0, length);
+        
+        // 최종 결과가 1 or 0 이면 하나로 압축 가능하다는 의미
         if (res == 0 || res == 1) answer[res]++;
         
         return answer;
@@ -29,7 +31,8 @@ class Solution {
             return 0;
         }
         
-        // 압축 불가능한 경우, 그 상위도 이제 압축 불가능
+        // 압축 불가능한 경우, 그 상위도 이제 앞으로 쭉 압축 불가능
+        // 그래서 현재까지 결과를 정답에 반영한 후 진행
         if (lu == 0 || lu == 1) answer[lu]++;
         if (ru == 0 || ru == 1) answer[ru]++;
         if (ld == 0 || ld == 1) answer[ld]++;
