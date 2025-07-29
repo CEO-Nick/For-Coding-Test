262e7e17fdfdf84b41aef3c3f7c0c289051479da
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-29 09:42:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 41.57 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,50 +1,45 @@
 import java.util.*;
 
 class Solution {
+    // 현재 큐의 합이 큰 쪽에서 pop해서 다른 쪽에 insert 하기
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
-        long sum = 0L;
-        long sum1 = 0L;
-        long sum2 = 0L;
-        int len = queue1.length;
-        Queue<Integer> q1 = new ArrayDeque<>();
-        Queue<Integer> q2 = new ArrayDeque<>();
-        for (int i = 0; i < len; i++) {
-            sum += queue1[i] + queue2[i];
+        int answer = 0;
+        long sum1 = 0;
+        long sum2 = 0;
+        ArrayDeque<Integer> q1 = new ArrayDeque<>();
+        ArrayDeque<Integer> q2 = new ArrayDeque<>();
+        
+        for (int i = 0; i < queue1.length; i++) {
+            q1.offer(queue1[i]);
             sum1 += queue1[i];
+        }
+        for (int i = 0; i < queue2.length; i++) {
+            q2.offer(queue2[i]);
             sum2 += queue2[i];
-            q1.add(queue1[i]);
-            q2.add(queue2[i]);
         }
         
-        long std = sum / 2L;
-        int count = 0;
-        int num = 0;
         while (true) {
-            if (sum1 == sum2) {
-                return count;
-            }
-            
-            if (count > 600000) return -1;
-            
-            if (sum1 > std) {
-                num = q1.poll();
-                if (num > std) return -1;   // 원소 1개가 두 큐의 합의 절반보다 크면 각 큐의 원소 합을 같게 만들 수 없음
-                sum1 -= num;
-                
-                q2.add(num);
-                sum2 += num;
-            } else {
-                num = q2.poll();
-                if (num > std) return -1;
-                
-                sum2 -= num;
-                
-                q1.add(num);
+            if (sum1 < sum2) {
+                int num = q2.poll();
+                q1.offer(num);
                 sum1 += num;
+                sum2 -= num;
+                answer++;
+            } else if (sum1 > sum2) {
+                int num = q1.poll();
+                q2.offer(num);
+                sum1 -= num;
+                sum2 += num;
+                answer++;
+            } else {
+                break;
             }
-            count++;
+            
+            if (answer >= 700000) {
+                answer = -1;
+                break;
+            }
         }
-        
+        return answer;
     }
 }
