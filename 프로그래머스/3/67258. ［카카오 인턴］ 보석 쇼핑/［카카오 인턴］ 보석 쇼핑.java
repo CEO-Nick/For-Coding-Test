5975b0afd53bb9d8e9e24ed390b8739a10106574
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-07-31 10:06:25 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 52.55 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,30 +1,58 @@
 import java.util.*;
 
 class Solution {
+    // 진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매
+    
+    // 우선 보석 종류 개수를 알기 위해선 HashSet을 활용
+    // 투 포인터 앞에서부터 시작(같은 길이인 경우 시작 진열대 작은 구간을 찾아야 되니까)
+    
+    // 첫 시작부터 모든 종류 파악 가능한 
     public int[] solution(String[] gems) {
-        HashSet<String> size = new HashSet<>(Arrays.asList(gems));
-        int max = size.size();
-        int l = 0, r = 0;
-        int al = 1, ar = gems.length;
-        int min = Integer.MAX_VALUE;
         
-        HashMap<String, Integer> map = new HashMap<>();
-        while (r < gems.length) {
-            int count = map.getOrDefault(gems[r], 0) + 1;
-            map.put(gems[r++], count);
-            while (map.size() == max) {
-                if (min > r - l) {
-                    min = r - l;
-                    al = l + 1;
-                    ar = r;
+        HashSet<String> all = new HashSet<>();
+        
+        for (String gem : gems) all.add(gem);
+        int total = all.size();
+        
+        HashMap<String, Integer> current = new HashMap<>();
+        
+        
+        
+        int l = 0; int r = 0;
+        for (int i = 0; i < gems.length; i++) {
+            current.put(gems[i], current.getOrDefault(gems[i], 0) + 1);
+            
+            if (current.size() == total) {
+                r = i;
+                break;
+            }
+        }
+        int minLen = r - l + 1;
+        int[] answer = new int[] {l+1, r+1};
+        
+        while (l <= r) {
+            // 현재 모든 종류 포함하고 있다면
+            if (current.size() == total) {
+                // 최신화 가능한지 확인하고
+                if (r-l+1 < minLen) {
+                    minLen = r-l+1;
+                    answer[0] = l+1;
+                    answer[1] = r+1;
                 }
-                count = map.get(gems[l]) - 1;
-                if (count == 0) map.remove(gems[l]);
-                else map.put(gems[l], count);
+                // l를 오른쪽으로 한 칸 이동
+                int count = current.get(gems[l]);
+                if (count == 1) current.remove(gems[l]);
+                else current.put(gems[l], count-1);
                 l++;
+            } else {
+                r++;
+                if (r >= gems.length) break;
+                current.put(gems[r], current.getOrDefault(gems[r], 0)+1);
             }
             
         }
-        return new int[] {al, ar};
+        
+        
+        return answer;
     }
 }
