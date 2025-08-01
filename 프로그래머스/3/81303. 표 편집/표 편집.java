cead96d36909bd08ba82dc5d6263e22530a264ae
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-08-01 10:18:43 +0900

    [level 3] Title: 표 편집, Time: 342.45 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,44 +1,86 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int n, int k, String[] cmd) {
-        int[] up = new int[n+2];
-        int[] down = new int[n+2];
+    static class Info {
+        int idx;
+        int left;
+        int right;
         
-        for (int i = 0; i < n+2; i++) {
-            up[i] = i - 1;
-            down[i] = i + 1;
+        Info(int idx, int left, int right) {
+            this.idx = idx;
+            this.left = left;
+            this.right = right;
         }
+    }
+    
+    public String solution(int n, int k, String[] command) {
+        int[][] linkedList = new int[n][2];
         
-        ArrayDeque<Integer> delete = new ArrayDeque<>();
-        k++;
+        for (int i = 0; i < n; i++) {
+            linkedList[i][0] = i-1;
+            linkedList[i][1] = i+1;
+        }
+        linkedList[n-1][1] = -1;
         
-        for (String c : cmd) {
-            if (c.startsWith("C")) {
-                delete.push(k);
-                up[down[k]] = up[k];
-                down[up[k]] = down[k];
-                k = n < down[k] ? up[k] : down[k];
-            }       
-            else if (c.startsWith("Z")) {
-                int restore = delete.pop();
-                down[up[restore]] = restore;
-                up[down[restore]] = restore;
-            } else {
-                String[] s = c.split(" ");
-                int x = Integer.valueOf(s[1]);
+        
+        Stack<Info> stack = new Stack<>();
+        
+        for (String cmd : command) {
+            if (cmd.startsWith("U")) {
+                String[] parts = cmd.split(" ");
+                int move = Integer.parseInt(parts[1]);
+                for (int i = 0; i < move; i++) {
+                    k = linkedList[k][0];
+                }
                 
-                for (int i = 0; i < x; i++) {
-                    k = s[0].equals("U") ? up[k] : down[k];
+            } else if (cmd.startsWith("D")) {
+                String[] parts = cmd.split(" ");
+                int move = Integer.parseInt(parts[1]);
+                for (int i = 0; i < move; i++) {
+                    k = linkedList[k][1];
+                }
+                
+            } else if (cmd.equals("C")) {
+                int left = linkedList[k][0];
+                int right = linkedList[k][1];
+                
+                stack.push(new Info(k, left, right));
+
+                if (left == -1) {
+                    linkedList[right][0] = -1;
+                    k = right;
+                } else if (right == -1) {
+                    linkedList[left][1] = -1;
+                    k = left;
+                } else {
+                    linkedList[left][1] = right;
+                    linkedList[right][0] = left;
+                    k = right;
+                }
+            } else {
+                Info info = stack.pop();
+                if (info.left == -1) {
+                    linkedList[info.right][0] = info.idx;
+                } else if (info.right == -1) {
+                    linkedList[info.left][1] = info.idx;
+                } else {
+                    linkedList[info.left][1] = info.idx;
+                    linkedList[info.right][0] = info.idx;
                 }
             }
         }
-        
-        char[] answer = new char[n];
-        Arrays.fill(answer, 'O');
-        for (int idx : delete) {
-            answer[idx - 1] = 'X';
+       
+        boolean[] checkList = new boolean[n];
+        Arrays.fill(checkList, true);
+        for (Info info : stack) {
+            checkList[info.idx] = false;
         }
-        return new String(answer);
+
+        StringBuilder sb = new StringBuilder();
+        for (boolean exist : checkList) {
+            if (exist) sb.append("O");
+            else sb.append("X");
+        }
+        return sb.toString();
     }
 }
