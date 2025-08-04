e1edf450605525caf144f209b73328a841076c7e
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-08-05 08:48:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1596 ms, Memory: 81872 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,45 +1,51 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static List<Integer>[] tree;
-    public static boolean[] check;
+    public static boolean[] visited;
     public static int[] parents;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         tree = new ArrayList[n+1];
-        for (int i = 0; i < n + 1; i++) {
-            tree[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < n - 1; i++) {
-            int node1 = sc.nextInt();
-            int node2 = sc.nextInt();
+        for (int i = 0; i <= n; i++) tree[i] = new ArrayList<>();
+
+        for (int i = 0; i < n-1; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int node1 = Integer.parseInt(inputs[0]);
+            int node2 = Integer.parseInt(inputs[1]);
+
             tree[node1].add(node2);
             tree[node2].add(node1);
         }
-        check = new boolean[n+1];
+
+        visited = new boolean[n+1];
         parents = new int[n+1];
 
+        // 1번 노드부터 순회하면서 각 노드의 부모 노드 저장
         traversal(1);
-        for (int i = 2; i < n + 1; i++) {
+
+        for (int i = 2; i <= n; i++) {
             System.out.println(parents[i]);
         }
+
     }
 
-    private static void traversal(int node) {
-        check[node] = true;
-        for (int i = 0; i < tree[node].size(); i++) {
-            int child = tree[node].get(i);
-            if (!check[child]) {
-                parents[child] = node;
-                traversal(child);
-            }
+    public static void traversal(int node) {
+        visited[node] = true;
+        for (int child : tree[node]) {
+            if (visited[child]) continue;
+            parents[child] = node;
+            visited[child] = true;
+            traversal(child);
         }
     }
-
 }
