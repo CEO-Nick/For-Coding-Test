c43864ba0f8d19c22a8213caa6a63025a3e8bfde
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-08-05 08:16:59 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,18 +1,33 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String doc = sc.nextLine();
-        String word = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        // 찾으려는 단어만큼 substring 으로 추출해서 비교
+        // 일치하면 해당 길이만큼 건너뛰기
+        // 다르면 다음으로 넘어가기
 
-        String replaced = doc.replace(word, "");
-        int length = doc.length() - replaced.length();
-        int count = length / word.length();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String doc = br.readLine();
+        String word = br.readLine();
+        int len = word.length();
+        int answer = 0;
 
-        System.out.println(count);
+        int idx = 0;
+        while (idx < doc.length()) {
+            int endIdx = Math.min(doc.length(), idx + len);
+            String temp = doc.substring(idx, endIdx);
+            if (word.equals(temp)) {
+                answer++;
+                idx += len;
+            } else {
+                idx++;
+            }
+        }
+
+        System.out.println(answer);
     }
 
 }
